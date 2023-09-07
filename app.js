75c92c1ba3874c3c51de1f88721ded8bcbf0e82c
Karim Altohamy <karimaltohamy@Karims-MacBook-Air.local>
2023-09-07 15:50:37 +0200

    commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://multi-vendor-ecommerce-theta.vercel.app",
     optionsSuccessStatus: 200, // For legacy browser support
   })
 );
