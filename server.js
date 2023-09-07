75a9b44c4160c026d0fb3185612b306dd865d2d2
Karim Altohamy <karimaltohamy@Karims-MacBook-Air.local>
2023-09-07 16:37:17 +0200

    commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 const conactionMongodb = require("./db/database");
 
-process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
-  console.log("shutting down the server for handling uncaught exception");
-});
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log("shutting down the server for handling uncaught exception");
+// });
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: "./config/.env" });
-}
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config({ path: "./config/.env" });
+// }
 
 app.get("/", (req, res) => {
   res.send("run");
@@ -21,11 +21,11 @@ const server = app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost/${process.env.PORT}`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log(`shutting down the server for ${err.message}`);
-  console.log("shutting down the server for unhandling promise rejection");
+// process.on("unhandledRejection", (err) => {
+//   console.log(`shutting down the server for ${err.message}`);
+//   console.log("shutting down the server for unhandling promise rejection");
 
-  server.close(() => {
-    process.exit(1);
-  });
-});
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
